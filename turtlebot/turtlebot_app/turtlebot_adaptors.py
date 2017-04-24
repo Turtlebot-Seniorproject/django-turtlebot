ab10955467c1ebc8a3b5dbaa81d9a7bfcd324375
dohertyj08 <jamesdoherty@outlook.com>
2017-04-24 08:47:43 -0400

    small updates

--- turtlebot/turtlebot_app/turtlebot_adaptors.py
+++ turtlebot/turtlebot_app/turtlebot_adaptors.py
@@ -1,14 +1,16 @@
 import socket
 
 
-def establish_connection_and_send_destination(location_id=None):
+def establish_connection_and_send_destination(location_id=None, ip=None):
     s = socket.socket()
-    host = "192.168.1.6"
+    if ip:
+        host = ip
+    else:
+        host = "192.168.1.6"
     port = 10888
     s.connect((host, port))
     if s.recv(1024) == "Connection established":
         if location_id:
             s.send(location_id)
         else:
-            s.send("ERROR: No Room Name")
-        print "message sent"
+            s.send("ERROR")
--- turtlebot/turtlebot_app/views.py
+++ turtlebot/turtlebot_app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from . import turtlebot_adaptors
+import csu_constants
 
 
 def home(request):
     if request.method == "POST":
         location_id = request.POST.get("destination", "")
-        turtlebot_adaptors.establish_connection_and_send_destination(location_id)    
+        ip = request.POST.get("IP", "")
+        location_id = _get_named_rooms(location_id)
+        turtlebot_adaptors.establish_connection_and_send_destination(location_id, ip)    
     return render(request, "turtlebot_app/home.html")
+
+def _get_named_rooms(location_id=None):
+    if location_id:
+        if location_id == "Foxes Den":
+            return "130"
+        #add other room definitions here.
+        #this could be a dictionary but I would like to add user-auth
+    else:
+        return "ERROR"
